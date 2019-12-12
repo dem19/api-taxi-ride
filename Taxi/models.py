86b17a163f4532e94527d34b980c316d18053ac5
dem19 <dem19vieira@gmail.com>
2019-12-12 08:45:01 -0300

    Nova

--- Taxi/models.py
+++ Taxi/models.py
@@ -1,17 +1,31 @@
 from django.db import models
 
-class CadTaxista(models.Model):
-    Nfuncionario = models.CharField(max_length=100)
-    Nempresa = models.CharField(max_length=100)
-    bairro = models.CharField(max_length=100)
-    rua = models.CharField(max_length=100)
-    numero = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=500)
 
 
+class EmpTaxi(models.Model):
+    nome_empresa = models.CharField(max_length=100)
+    CNPJ = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.Nempresa
+        return self.nome_empresa
+
+class CadTaxista(models.Model):
+    Nfuncionario = models.CharField(max_length=100)
+    Nempresa = models.ForeignKey(EmpTaxi, on_delete=models.CASCADE)
+    bairro = models.CharField(max_length=100)
+    rua = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=16)
+    RG = models.CharField(max_length=9)
+    CPF = models.CharField(max_length=14)
+    CNH_profissional = models.CharField()
+    placa = models.CharField(max_length=100)
+    cor_moto = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f' {self.Nfuncionario}'
+
+
+
 
 
 
